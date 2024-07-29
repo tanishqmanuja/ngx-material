f8490039d024b06cbbd7c89d408cb2d477b14c8c
tanishqmanuja <tanishqmanuja@gmail.com>
2024-07-29 17:35:22 +0530

    fix: tonal btn --> filled-tonal btn

--- src/material/button/tonal-button.component.ts
+++ src/material/button/tonal-button.component.ts
@@ -1,13 +1,13 @@
-import "@material/web/button/tonal-button.js";
+import "@material/web/button/filled-tonal-button.js";
 
 import { ChangeDetectionStrategy, Component } from "@angular/core";
 
 import { MdButtonComponent } from "./internal/button.component";
 
 @Component({
-  selector: "md-tonal-button",
+  selector: "md-filled-tonal-button",
   standalone: true,
   template: ` <ng-content />`,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MdTonalButtonComponent extends MdButtonComponent {}
+export class MdFilledTonalButtonComponent extends MdButtonComponent {}
